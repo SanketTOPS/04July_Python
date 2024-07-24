2930da5436096b54ba3dc6415e478cc62d2bf396
SanketTops <sanket.tops@gmail.com>
2024-07-24 18:26:10 +0530

    List

---
+++ Assignments/Module-2/list_example.py
@@ -0,0 +1,54 @@
+data=['c','c++','python','java','php']
+#print(data)
+"""print(data[0])
+print(data[-1])
+print(data[0:3])
+print(data[2:])
+print(data[:3])
+print(len(data))"""
+
+#data[3]="Android"
+#print(data)
+
+#-------------------------------------------------#
+#print(data)
+
+"""for i in data:
+    print(i)"""
+
+#print(data[0])
+#print(data.index("python"))
+
+#print(data)
+
+"""if 'c++' in data:
+    print("Yes...")
+else:
+    print("Nooo")"""
+
+# --------------------------------------------------- #
+
+#print(data)
+#data.append("html")
+#data.insert(1,"css")
+#data.remove("php")
+#data.pop()
+#data.pop(3)
+#del data[0]
+#data.clear()
+#data.reverse()
+#data.sort()
+#del data
+#print(data)
+
+#newdata=data.copy()
+#print("This is Newdata",newdata)
+
+print(data)
+
+newdata=["HTML","CSS","JS"]
+print(newdata)
+
+#print(data+newdata)
+data.extend(newdata)
+print(data)
